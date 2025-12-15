29f126a819445074e70c83b99711460c19352a0a
MATTHIAS SILBERHAIN <m.silberhain@mail.de>
2025-12-15 18:21:51 +0100

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,25 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-  const text = "MATTHIAS SILBERHAIN";
-  const target = document.getElementById("type-text");
-  const preloader = document.getElementById("preloader");
-  let i = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  var text = "MATTHIAS SILBERHAIN";
+  var target = document.getElementById("type-text");
+  var preloader = document.getElementById("preloader");
+  var i = 0;
 
   function type() {
+    if (!target) {
+      preloader.style.display = "none";
+      return;
+    }
+
     if (i < text.length) {
       target.textContent += text.charAt(i);
       i++;
       setTimeout(type, 70);
     } else {
-      setTimeout(() => {
-        preloader.style.opacity = "0";
-        setTimeout(() => preloader.remove(), 400);
-      }, 600);
+      setTimeout(function () {
+        preloader.style.display = "none";
+      }, 400);
     }
   }
 
   type();
 
-  const year = document.getElementById("jahr");
+  var year = document.getElementById("jahr");
   if (year) year.textContent = new Date().getFullYear();
 });
