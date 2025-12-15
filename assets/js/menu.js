dd20a9367f3ab2bf79346ca4b2fe2018a6d5e99c
MATTHIAS SILBERHAIN <m.silberhain@mail.de>
2025-12-15 16:32:13 +0100

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,16 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  /* PRELOADER TYPEWRITER */
+  const text = "MATTHIAS SILBERHAIN";
+  const textEl = document.getElementById("type-text");
+  const cursor = document.querySelector(".cursor");
   const preloader = document.getElementById("preloader");
 
-  setTimeout(() => {
-    preloader.style.opacity = "0";
-  }, 1800);
+  let i = 0;
+  const speed = 90;
 
-  setTimeout(() => {
-    preloader.style.display = "none";
-    document.body.classList.remove("preloader-active");
-  }, 2600);
+  const typing = setInterval(() => {
+    textEl.textContent += text.charAt(i);
+    i++;
 
+    if (i === text.length) {
+      clearInterval(typing);
+      cursor.classList.add("stop");
+
+      setTimeout(() => {
+        preloader.style.opacity = "0";
+      }, 600);
+
+      setTimeout(() => {
+        preloader.style.display = "none";
+        document.body.classList.remove("preloader-active");
+      }, 1200);
+    }
+  }, speed);
+
+  /* BURGER MENÜ */
   const burger = document.getElementById("burger");
   const navigation = document.getElementById("navigation");
 
@@ -18,6 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navigation.classList.toggle("aktiv");
   });
 
+  /* FOOTER JAHR */
   document.getElementById("jahr").textContent =
     new Date().getFullYear();
 });
